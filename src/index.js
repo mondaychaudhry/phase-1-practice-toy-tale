7d1b422cf276f68a337695e01d32d700fb0e4b9d
Monday <sobaata@gmail.com>
2021-08-02 11:55:05 -0400

    NaN Issue

--- src/index.js
+++ src/index.js
@@ -13,3 +13,66 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+const w = document.querySelector ('#toy-collection')
+fetch ('http://localhost:3000/toys').then(x=>x.json())
+.then(y=>y.forEach(z=>
+  a(z)))
+
+  function a (z)  {
+    let b = document.createElement('div')
+    b.className = 'card'
+    let d = document.createElement('h2')
+    d.innerText = z.name
+    let e= document.createElement('img')
+    e.src = z.image
+    e.className= 'toy-avatar'
+    let f = document.createElement('p')
+    f.innerText = `${z.likes} Likes`
+    let c = document.createElement ('button')
+    c.innerText= 'Like <3'
+    c.className= 'like-btn'
+    c.addEventListener ('click',(e) => {
+    const g = e.target.previousSibling.innerText 
+    const h = g.split('')[0]
+    e.target.previousSibling.innerText = `${parseInt(h) + 1} Likes`
+    fetch (`http://localhost:3000/toys/${z.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({likes: parseInt(h)+1})
+    })
+    })
+    w.appendChild(b)
+    b.append(d,e,f,c)
+    }
+
+    
+
+    document.querySelector('.add-toy-form').addEventListener ('submit', e => {
+      e.preventDefault()
+      const x = e.target.name.value
+      const y = e.target.image.value
+      const z = {
+        name: x,
+        image: y,
+        likes: 0 
+      }
+      a (z)
+      fetch (`http://localhost:3000/toys`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({
+        'name': `${x}`, 
+        'image':`${y}`,
+        'likes': 0
+      })
+    })
+    })
+
+
